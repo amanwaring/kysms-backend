be694cec4e8fe898f40f45b9bbdfabf6ce7e48dc
andrew <andrew.manwaring@gmail.com>
2024-09-11 23:04:33 -0600

    Add anthropic

--- src/handler.ts
+++ src/handler.ts
@@ -1,8 +1,19 @@
+import Anthropic from "@anthropic-ai/sdk";
 import { NewMessageWebhookBody } from "./dto/new-message-webhook-body";
+import { TextBlock } from "@anthropic-ai/sdk/resources/messages.mjs";
 
 export const newMessageWebhook = async (event) => {
   const body: NewMessageWebhookBody = JSON.parse(event.body);
-  const answer = `You said: ${body.message}. That's the answer.`;
+  const anthropic = new Anthropic();
+  const aiResponse = await anthropic.messages.create({
+    model: 'claude-3-haiku-20240307',
+    max_tokens: 1024,
+    messages: [{
+      role: 'user',
+      content: body.message
+    }]
+  })
+  const answer = (aiResponse.content[0] as TextBlock).text;
   return {
     statusCode: 200,
     body: answer,
